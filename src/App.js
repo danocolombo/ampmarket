0ae6dcefd196d3bfecc887e9c2a0ec616b82898e
danocolombo@google.com <GR0mans1212!>
2020-12-29 16:24:50 -0500

    not functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
+import React, { Component } from 'react';
+import { Auth, Hub } from 'aws-amplify';
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import MarketPage from './pages/MarketPage';
+import Navbar from './components/Navbar';
+
 import './App.css';
 
-const App = () => {
-    return (
-        <>
-            <div>App</div>
-        </>
-    );
-};
+class App extends Component {
+    state = {
+        user: null,
+    };
+
+    componentDidMount() {
+        console.log('AmplifyTheme object:');
+        console.dir(AmplifyTheme);
+        this.getUserData();
+        Hub.listen('auth', this.onHubCapsure);
+    }
+    getUserData = async () => {
+        const user = await Auth.currentAuthenticatedUser();
+        // user ? this.setState({ user: user }) : this.setState({ user: null });
+    };
+    onHubCapsure = (payload) => {
+        switch (payload.event) {
+            case 'signIn':
+                console.log('signed in');
+                this.getUserData();
+                break;
+            case 'signUp':
+                console.log('signed up');
+                break;
+            case 'signOut':
+                console.log('signed out');
+                this.setState({ user: null });
+                break;
+            default:
+                return;
+        }
+    };
+    render() {
+        const { user } = this.state;
+        return !user ? (
+            <Authenticator theme={theme} />
+        ) : (
+            <Router>
+                <>
+                    {/* Nav Bar */}
+                    <Navbar user={user} />
+                    {/* Routes */}
+                    <div className='app-container'>
+                        <Route exact path='/' component={HomePage} />
+                        <Route path='/profile' component={ProfilePage} />
+                        {/* <Route
+                            path='/markets/:marketId'
+                            component={({ match }) => (
+                                <MarketPage marketId={match.params.marketId} />
+                            )}
+                        /> */}
+                    </div>
+                </>
+            </Router>
+        );
+    }
+}
 // to customize cognito interfaces (login, register, etc.)
 // we imported the default them above on te import
 const theme = {
@@ -29,4 +86,5 @@ const theme = {
         color: '#ffffff',
     },
 };
-export default withAuthenticator(App, true, [], null, theme);
+//export default withAuthenticator(App, true, [], null, theme);
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,35 @@
-import React from "react";
-// import { Menu as Nav, Icon, Button } from "element-react";
-
-const Navbar = () => <div>Navbar</div>;
-
+import React from 'react';
+import { Menu as Nav, Icon, Button } from 'element-react';
+import { NavLink } from 'react-router-dom';
+const Navbar = ({ user }) => (
+    <Nav mode='horizontal' themen='dark' defaultActive='1'>
+        <div className='nav-container'>
+            <Nav.Item index='1'>
+                <NavLink to='/' className='nav-link'>
+                    <span className='app-title'>
+                        <img
+                            src='https://icon.now.sh/account_balance/f90'
+                            alt='App Icon'
+                            className='app-icon'
+                        />
+                    </span>
+                </NavLink>
+            </Nav.Item>
+            <div className='nav-items'>
+                <Nav.Item index='2'>
+                    <span className='app-user'>Hello, {user.username} </span>
+                </Nav.Item>
+                <Nav.Item index='3'>
+                    <NavLink path='/profile' className='nav-link'>
+                        <Icon name='setting' />
+                        Profile
+                    </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <Button type='warning'>Sign Out</Button>
+                </Nav.Item>
+            </div>
+        </div>
+    </Nav>
+);
 export default Navbar;
