b3be5cfe77119509456518a6dd311aa9b9967457
danocolombo@google.com <GR0mans1212!>
2020-12-29 08:38:19 -0500

    generic withAuthenticator implementaotion

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -1,11 +1,33 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
+import './App.css';
 
 const App = () => {
     return (
-      <>
-    <div>AppHooks</div>;
-    </>)
-}
-
-export default App;
+        <>
+            <div>AppHooks</div>
+        </>
+    );
+};
+// to customize cognito interfaces (login, register, etc.)
+// we imported the default them above on te import
+const theme = {
+    ...AmplifyTheme,
+    //change button color (find by doing inspect when viewing modal)
+    button: {
+        ...AmplifyTheme.button,
+        backgroundColor: 'var(--amazonOrange)',
+    },
+    sectionHeader: {
+        ...AmplifyTheme.sectionHeader,
+        backgroundColor: 'var(--darkBlue)', //this is using the styles already loaded
+    },
+    // Navbar tweaks
+    navBar: {
+        ...AmplifyTheme.navBar,
+        backgroundColor: 'var(--grey)',
+        backgroundColor: '#3333ff',
+        color: '#ffffff',
+    },
+};
+export default withAuthenticator(App, true, [], null, theme);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./AppHooks";
-import * as serviceWorker from "./serviceWorker";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 // Bring in default Element React theme
-import "element-theme-default";
+import 'element-theme-default';
+import App from './AppHooks';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import * as serviceWorker from './serviceWorker';
+//the following configure line has to be BELOW all imports
+Amplify.configure(aws_exports);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
