f5ddf659899f98174a241db761e6b2dc61c63532
danocolombo <danocolombo@gmail.com>
2020-12-29 06:54:55 -0500

    converted to function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./AppHooks";
 import * as serviceWorker from "./serviceWorker";
 
 // Bring in default Element React theme
